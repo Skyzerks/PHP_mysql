de1435a6ab7c1aa70a1963b71b80aab81d983452
SKYZERKS\Nemo <kwantum03@gmail.com>
2017-03-15 18:51:26 +0200

    Remodelling few controllers

--- controllers/productController.php
+++ controllers/productController.php
@@ -1,3 +1,28 @@
 <?php
+if( $_action == 'product' && $_id ) {
 
-include 'templates/productView.php';
+    $product = sql($pdo,
+        'SELECT * FROM `products` WHERE `id` = ' . $_id,
+        [],
+        'rows'
+    );
+
+    echo '<h1>' . $product[0]['title'] . '</h1>';
+    echo '<p>' . $product[0]['description'] . '</p>';
+    echo '<p>Price: ' . $product[0]['price'] . '</p>';
+
+
+include_once 'templates/productView.php';
+
+
+    function buy_product($id)
+    {
+        $_SESSION['basket'][]= $id;
+        echo 'Added to basket';
+    }
+    if (isset($_POST['btn']))
+        buy_product($_id);
+
+}
+
+include_once 'templates/productView.php';
--- templates/productView.php
+++ templates/productView.php
@@ -1,31 +1,6 @@
-<?php
 
-if( $_action == 'product' && $_id ) {
-
-    $product = sql($pdo,
-        'SELECT * FROM `products` WHERE `id` = ' . $_id,
-        [],
-        'rows'
-    );
-
-    echo '<h1>' . $product[0]['title'] . '</h1>';
-    echo '<p>' . $product[0]['description'] . '</p>';
-    echo '<p>Price: ' . $product[0]['price'] . '</p>';
-?>
 <!--    echo '<button>Buy</button>';-->
 
     <form method='post'>
         <input type='submit' value='Buy' name='btn'>
     </form>
-
-<?php
-
-    function buy_product($id)
-    {
-        $_SESSION['basket'][]= $id;
-        echo 'Added to basket';
-    }
-    if (isset($_POST['btn']))
-        buy_product($_id);
-
-}
